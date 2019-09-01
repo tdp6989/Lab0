e80f657f83a44ec7164ab01892f1118c22458d89
Troy <troypotter99@gmail.com>
2019-09-01 18:40:35 -0400

    Changed square.py to print two squares and added the README txt file

---
+++ README.txt
@@ -0,0 +1,3 @@
+Author: Troy Potter
+
+The program square.py will ask a user for a side length then draw two squares with the same length
--- square.py
+++ square.py
@@ -6,15 +6,31 @@ I wasnt quite sure where to find the square.py code, so I wrote it
 """
 import turtle
 
-sideLength = int(input('Enter a side length for this square: '))
+def drawSquare(sideLength):
+    """
+    :param sideLength: Length of square side
+    : Draws square
+    :return:
+    """
+    turtle.forward(sideLength)
+    turtle.left(90)
+    turtle.forward(sideLength)
+    turtle.left(90)
+    turtle.forward(sideLength)
+    turtle.left(90)
+    turtle.forward(sideLength)
+    turtle.left(90)
 
-turtle.forward(sideLength)
-turtle.left(90)
-turtle.forward(sideLength)
-turtle.left(90)
-turtle.forward(sideLength)
-turtle.left(90)
-turtle.forward(sideLength)
-turtle.left(90)
+    """
+    Moves the turtle to the right of the first drawn square by 25
+    """
+    turtle.up()
+    turtle.forward(sideLength + 25)
+    turtle.down()
+
+
+sideLength = int(input('Enter a side length for this square: '))
+drawSquare(sideLength)
+drawSquare(sideLength)
 
 turtle.done()
